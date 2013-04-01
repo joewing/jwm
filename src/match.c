22d990ed6053261ec78c9c7258ff61621f38ef42
Joe Wingbermuehle <joewing@joewing.net>
2013-04-01 09:45:21 -0500

    Documentation updates.

--- src/match.c
+++ src/match.c
@@ -17,9 +17,9 @@
 char Match(const char *pattern, const char *expression)
 {
 
-	regex_t re;
-	regmatch_t rm;
-	int rc;
+   regex_t re;
+   regmatch_t rm;
+   int rc;
 
    if(!pattern && !expression) {
       return 1;
@@ -27,15 +27,15 @@ char Match(const char *pattern, const char *expression)
       return 0;
    }
 
-	if(regcomp(&re, pattern, REG_EXTENDED) != 0) {
-		return 0;
-	}
+   if(regcomp(&re, pattern, REG_EXTENDED) != 0) {
+      return 0;
+   }
 
-	rc = regexec(&re, expression, 0, &rm, 0);
+   rc = regexec(&re, expression, 0, &rm, 0);
 
-	regfree(&re);
+   regfree(&re);
 
-	return rc == 0 ? 1 : 0;
+   return rc == 0 ? 1 : 0;
 
 }
 

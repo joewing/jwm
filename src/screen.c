cbca2bcfd9e166392cb7a7a16df4721aef13e74b
joewing <joewing@joewing.net>
2013-05-31 19:08:35 -0500

    Fix an issue when determining the screen given a pixel coordinate.

--- src/screen.c
+++ src/screen.c
@@ -85,7 +85,9 @@ const ScreenType *GetCurrentScreen(int x, int y)
    int index;
 
    x = Max(0, x);
+   x = Min(x, rootWidth);
    y = Max(0, y);
+   y = Min(y, rootHeight);
    for(index = 1; index < screenCount; index++) {
       sp = &screens[index];
       if(x >= sp->x && x < sp->x + sp->width) {

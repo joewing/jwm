42b2518cbf544599dc1799034a3c8dfbfec08783
Joe Wingbermuehle <joewing@joewing.net>
2015-09-04 09:23:45 -0500

    Portability improvements.

--- src/debug.h
+++ src/debug.h
@@ -31,12 +31,21 @@
 #   include <stdlib.h>
 #   ifdef HAVE_ALLOCA_H
 #      include <alloca.h>
+#   elif defined __GNUC__
+#      define alloca __builtin_alloca
+#   elif defined _AIX
+#      define alloca __alloca
+#   elif defined _MSC_VER
+#      include <malloc.h>
+#      define alloca _alloca
+#   elif defined HAVE_ALLOCA
+void *alloca(size_t);
 #   endif
 #endif /* MAKE_DEPEND */
 
 void Debug(const char *str, ...);
 
-#ifdef HAVE_ALLOCA_H
+#ifdef HAVE_ALLOCA
 
 #   define AllocateStack( x ) alloca( x )
 #   define ReleaseStack( x ) ((void)0)

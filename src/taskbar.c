6e9eb88a2f463d9175d8ab345eb459bd60470aba
Andrew Chadwick <61299+achadwick@users.noreply.github.com>
2025-03-15 22:13:15 +0000

    taskbar: desktop windows no longer prevent switch.
    Desktop-type windows no longer prevent the usual desktop switching
    behaviour when clicking on taskbar buttons if only other desktops
    contain any normal-type windows from the same group.
    
    Fixes joewing/jwm#654.

--- src/taskbar.c
+++ src/taskbar.c
@@ -431,9 +431,13 @@ void FocusGroup(const TaskEntry *tp)
    }
 
    /* If there is a client in the group on this desktop,
-    * then we remain on the same desktop. */
+    * then we remain on the same desktop. However,
+    * desktop windows in the group do not count. */
    shouldSwitch = 1;
    for(cp = tp->clients; cp; cp = cp->next) {
+      if (cp->client->state.windowType == WINDOW_TYPE_DESKTOP) {
+         continue;
+      }
       if(IsClientOnCurrentDesktop(cp->client)) {
          shouldSwitch = 0;
          break;

9b6b46443bcbd9eeb07985a6e65e9c5516dd827b
Joe Wingbermuehle <joewing@joewing.net>
2026-05-06 10:34:53 -0500

    NLS fixes.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
+MACOS_M4_DIR=/opt/homebrew/share/gettext/m4
+
 aclocal
 automake -ac
-autoreconf -fi
+
+if [ -e $MACOS_M4_DIR ] ; then
+   autoreconf -fi -I $MACOS_M4_DIR
+else
+   autoreconf -fi
+fi
+
 touch config.rpath
